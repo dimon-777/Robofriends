acf2b58587786f4e564840f163aa1ce057db5935
dimon-777 <dimon_85@hotmail.com>
2019-02-09 16:44:13 -0500

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import './index.css';
 import App from "./containers/App";
 import * as serviceWorker from './serviceWorker';
+import { saerchRobots } from './reducers';
 import "tachyons";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(saerchRobots)
+
+ReactDOM.render(
+                <Provider store={store} >
+                    <App />
+                </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
